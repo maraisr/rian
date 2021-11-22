92ebf7a7f16b15f80e25d077d7c215ea4b563fe5
Marais Rossouw <me@marais.co>
2021-11-22 22:48:59 +1000

    feat: init

---
+++ packages/rian/src/index.ts
@@ -0,0 +1,110 @@
+import type { Traceparent } from "tctx";
+import { make, parse } from "tctx";
+
+export type Span = {
+  name: string;
+  id: Traceparent;
+  parent?: Traceparent;
+  start: number;
+  end: number;
+  attributes: AllowedObject;
+};
+
+export type Agent = (spans: ReadonlySet<Span>) => any;
+
+type AllowedObject = Record<string, string | number | boolean | undefined>;
+
+export type Options = {
+  agent: Agent;
+  traceparent?: Traceparent;
+};
+
+interface Scope {
+  traceparent: Traceparent;
+
+  fork(name: string, traceparent?: Traceparent): Scope;
+
+  measure<Fn extends (scope: Scope) => any>(
+    name: string,
+    fn: Fn
+  ): ReturnType<Fn>;
+
+  setAttributes(tags: AllowedObject): void;
+
+  end(): void;
+}
+
+type ScopeParent = Scope;
+
+export const create = (name: string, options: Options): ScopeParent => {
+  const spans: Set<Span> = new Set();
+  const promises = [];
+
+  const scope = (name: string, parent?: Traceparent): Scope => {
+    const me = parent ? parent.child() : make();
+    const attributes: AllowedObject = {};
+
+    const start = performance.now();
+
+    return {
+      get traceparent() {
+        return me;
+      },
+      fork(name) {
+        return scope(name, me);
+      },
+      measure(name, cb) {
+        const scope = this.fork(name);
+        const set_error = (err: Error) => {
+          scope.setAttributes({
+            "error.message": err.message,
+            "error.stack": err.stack,
+          });
+        };
+
+        try {
+          const r = cb(scope);
+
+          if (r instanceof Promise)
+            promises.push(r.catch(set_error).finally(() => scope.end()));
+
+          return r;
+        } catch (e) {
+          set_error(e);
+          throw e;
+        } finally {
+          scope.end();
+        }
+      },
+      setAttributes(attr) {
+        Object.assign(attributes, attr);
+      },
+      end() {
+        spans.add({
+          id: me,
+          parent,
+          start,
+          end: performance.now(),
+          name,
+          attributes,
+        });
+      },
+    };
+  };
+
+  const me = scope(
+    name,
+    typeof options.traceparent === "string"
+      ? parse(options.traceparent)
+      : options.traceparent
+  );
+  const meEnd = me.end;
+
+  me.end = async () => {
+    await Promise.all(promises);
+    meEnd();
+    return options.agent(spans);
+  };
+
+  return me;
+};
---
+++ packages/rian/test/rian.spec.ts
@@ -0,0 +1,40 @@
+import { test } from "uvu";
+import * as assert from "uvu/assert";
+import { spy, restoreAll } from "nanospy";
+
+import * as rian from "../src";
+
+test.after.each(() => {
+  restoreAll();
+});
+
+test("exports", () => {
+  assert.type(rian.create, "function");
+});
+
+test("simple", () => {
+  const agent = spy();
+  const tracer = rian.create("simple", {
+    agent: console.log,
+  });
+
+  const scope = tracer.fork("some-name");
+
+  scope.setAttributes({
+    baz: "qux",
+  });
+
+  scope.measure("test", (scope) => {
+    scope.setAttributes({
+      foo: "bar",
+    });
+
+    return "test";
+  });
+
+  scope.end();
+
+  tracer.end();
+});
+
+test.run();
