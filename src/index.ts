9eb4d71b03930f9f15c335d376a225b4ff094047
Marais Rossouw <me@marais.co>
2022-10-14 18:37:59 +1000

    chore: use `.bind`

--- src/index.ts
+++ src/index.ts
@@ -74,7 +74,7 @@ export const create = (name: string, options: Options): Tracer => {
 			if (span_obj.end == null) span_obj.end = Date.now();
 		};
 
-		$.__add_promise = (p) => void promises.add(p);
+		$.__add_promise = promises.add.bind(promises);
 
 		return $;
 	};
